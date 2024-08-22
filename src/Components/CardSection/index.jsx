36fb5034f2ba62ea34e8768255d553ac0ea804c8
John <johnlouisstephenson@gmail.com>
2024-08-22 15:26:22 +0100

    Grid container for cards

--- src/Components/CardSection/index.jsx
+++ src/Components/CardSection/index.jsx
@@ -4,20 +4,26 @@ import bgDesktop from "../../assets/images/bg-main-desktop.png";
 import "./styles.css";
 
 const CardSection = () => {
+  return (
+    <div className="grid-container">
+      <div className="bg-gradient"></div>
+      <div className="card-front"></div>
+      <div className="card-back"></div>
+    </div>
+  );
 
-    return (
-    <div className="border-box">
-        <img src={bgDesktop} alt="background color gradient" className="background" />
-        <div className="card-front">
-            
-            <div className="white-ball"></div>
-            <div className="hollow-ball"></div>
-        </div>  
-        <div className="card-back">  
-         
-        </div>
-    </div>)
-}
+  // return (
+  // <div className="border-box">
+  //     <img src={bgDesktop} alt="background color gradient" className="background" />
+  //     <div className="card-front">
 
+  //         <div className="white-ball"></div>
+  //         <div className="hollow-ball"></div>
+  //     </div>
+  //     <div className="card-back">
+
+  //     </div>
+  // </div>)
+};
 
 export default CardSection;
